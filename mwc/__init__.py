c7cd54e05c774c7af0df2f1f944e5fc8db197ff6
Griffin Chure <gchure@caltech.edu>
2018-02-11 15:48:55 -0800

    Moving over to delbruck

--- mwc/__init__.py
+++ mwc/__init__.py
@@ -2,20 +2,16 @@
 
 """Top level package for MWC utilities"""
 
-<<<<<<< HEAD
+
 # from . import mcmc
 from . import viz
 from . import flow
 # from . import image
 from . import stats
 from . import model
-=======
-from . import viz
-
 
 
 from . import process
->>>>>>> 35d86de573344fdd64cfc01d1bfdf2097cca1365
 
 
 __author__ = """Griffin Chure"""
