7c2f6d494770afa66d07753f35cbaea90a929d55
Griffin Chure <gchure@caltech.edu>
2018-02-11 17:40:19 -0800

    Made small change to segmentation function

--- code/processing/20180209_tenjin_37C_glucose_O2_dilution/segmentation.m
+++ code/processing/20180209_tenjin_37C_glucose_O2_dilution/segmentation.m
@@ -4,24 +4,7 @@ addpath(genpath('../../../../SuperSegger'));
 % Define the experiment parameters.
 DATE = '20180209';
 BASENAME = 'tenjin_37C_glucose_O2';
-
-% Get the snaps names.
-snap_files = dir(['../../../data/images/', DATE, '_', BASENAME,...
-    '_dilution/', DATE, '*_snaps*']);
-snap_samples = {snap_files.name};
-samples = {[DATE, '_growth_0'], [DATE,'_growth_1'], [DATE, '_growth_2']};
-
-ignored = {'.', '..', '.DS_Store'};
-for i=1:length(snap_samples)
-
-    valid = 0;
-    for j=1:length(ignored)
-        valid = valid + strcmp(snap_samples{i}, ignored{j});
-    end
-    if valid == 0
-        samples{end+1} = snap_samples{i};
-        end
-end
+samples = {'snaps', 'growth_0', 'growth_1', 'growth_2'}
 
 CONST = loadConstants('100XEc');
 CONST.parallel.PARALLEL_FLAG = 1;
@@ -29,7 +12,7 @@ CONST.trackFoci.numSpots = 0;
 CONST.align.ALIGN_FLAG = 1;
 CONST.trackOpti.REMOVE_STRAY = 1;
 cleanFlag = 0;
-samples = snap_samples;
+
 for i=1:length(samples)
     disp(samples{i})
     statement = ['Beginning segmentaton ', num2str(i), ' out of ',...
