8d0adc500f25221809775624206e01f5ee6904e5
Griffin Chure <gchure@caltech.edu>
2018-02-15 10:27:25 -0800

    Added some doc files as well as small changes to functions

--- mwc/io.py
+++ mwc/io.py
@@ -1,16 +1,37 @@
 import numpy as np
 import skimage.io
+import paramiko
 import scp
+import yaml
 import os
 
 
-def push_delbruck(src, dest, username, port):
+def fetch_status(dir, file='README.md'):
+    """
+    Reads the status of a given experimental dataset. This status is embedded
+    in the README.md file as a YAML metadata block.
+    """
     return True
 
 
-def push_aws(src, dest, username):
+def pull_clist(src, dest=None, username, port):
+    """
+    Pulls the clist files from a target directory on delbrück to local.
+
+    Parameters
+    ----------
+    src: str
+        Source directory on Delbrück. This should be given as a relative path.
+    dst: str
+        Target directory on local. If None, the clists will be pulled into
+        `data/images` with the same folder name.
+    username: str
+        Username to access the data.
+    port : int
+        Super secret Delbrück port number.
+    """
     return True
 
 
-def push_spicybeef(src, dest, port, username):
+def deoposit_data(src, dest, port, username):
     return True
