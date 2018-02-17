238fe956c0ecdd6a010f17fcfcab01623c80cfe2
Griffin Chure <gchure@caltech.edu>
2018-02-16 17:07:53 -0800

    Moving estimation writeup to delbruck for MCMC

--- figs/dilution_derivation.py
+++ figs/dilution_derivation.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pymc3
+import pymc3 as pm
 import scipy.stats
 import scipy.special
 import sys
@@ -15,58 +15,131 @@ mwc.viz.personal_style()
 %matplotlib inline
 
 
+#%% Noise-free simulation.
 # Set up parameters
-num_div = 1000
-alpha_seed = 150
+num_div = 100
+alpha_true = 150
 
-# Distribute proteins via gamma.
-n_tot = np.random.gamma(2, 40, size=num_div).astype(int)
+# Set the protein number range.
+prot_range = np.arange(10, 1000, 5)
 
-# Binomially partition into daughters.
-n_1 = np.random.binomial(n_tot, p=0.5)
-n_2 = n_tot - n_1
+# Set up a dataframe.
+dfs = []
+cols = ['summed', 'fluct', 'n1', 'n2', 'ntot']
+for i, ntot in enumerate(prot_range):
+    n1 = np.random.binomial(ntot, p=0.5, size=num_div)
+    n2 = ntot - n1
+    summed = alpha_true * ntot
+    fluct = (alpha_true * n1 - alpha_true * n2)**2
+    copy_dict = dict(summed=summed, fluct=fluct, n1=n1, n2=n2, ntot=ntot)
+    df = pd.DataFrame(np.array([np.ones(num_div) * summed, fluct, n1, n2,
+                                np.ones(num_div) * ntot]).T, columns=cols)
+    dfs.append(df)
+df = pd.concat(dfs, ignore_index=True)
 
-# Convert to intensity using the alpha seed value.
-I_1 = alpha_seed * n_1
-I_2 = alpha_seed * n_2
+#%% Plot the simulation data.
+# Compute the averages.
+grouped = df.groupby('ntot').mean()
 
-# Assemble data in a dataframe for easy manipulation.
-simple_df = pd.DataFrame(np.array((I_1, I_2, I_1 + I_2, (I_1 - I_2)**2)).T,
-                         columns=['I_1', 'I_2', 'summed', 'fluct'])
-simple_df.insert(3, 'alpha', alpha_seed)
-
-
-# Bin the data by 50 events.
-sorted_vals = simple_df.sort_values('summed')
-bin_size = 50
-bins = np.arange(0, len(sorted_vals) + bin_size, bin_size)
-
-# Compute the means.
-mean_summed, mean_fluct = [], []
-for i in range(1, len(bins)):
-    d = sorted_vals.iloc[bins[i - 1]:bins[i] + 1].mean()
-    mean_summed.append(d['summed'])
-    mean_fluct.append(d['fluct'])
-
-# Compute the theoretical prediction.
-I_tot_range = np.logspace(2, 5, 500)
-
-theory = alpha_seed * I_tot_range
-
-# Set up the figure canvas.
-fig, ax = plt.subplots(1, 1, figsize=(4, 3))
-ax.set_xscale('log')
+# Compute the theory
+theory = alpha_true * (alpha_true * prot_range)
+fig, ax = plt.subplots(figsize=(5, 4))
 ax.set_yscale('log')
-ax.set_xlabel('$I_1 + I_2$ [a.u.]')
-ax.set_ylabel('$(I_1 - I_2)^2$ [a.u.]')
-
-# Plot the data, bins, and theory.
-_ = ax.plot(simple_df['summed'], simple_df['fluct'], '.', ms=1,
-            label='simulated data')
-_ = ax.plot(mean_summed, mean_fluct,  '.', ms=5, label='binned data')
-_ = ax.plot(I_tot_range, theory, '-', label=r'$\alpha I_\mathrm{tot}$')
-
-# Format
-ax.set_xlim([500, 1E5])
+ax.set_xscale('log')
+ax.set_xlabel('$I_1 + I_2$')
+ax.set_ylabel('$(I_1 + I_2)^2$')
+_ = ax.plot(df['summed'], df['fluct'], '.', ms=0.5, label='simulated data')
+_ = ax.plot(grouped['summed'], grouped['fluct'], '.', ms=4, label='averages')
+_ = ax.plot(alpha_true * prot_range, theory, label='prediction')
 _ = ax.legend()
-# sns.despine(offset=5)
+sns.despine(offset=7)
+plt.tight_layout()
+plt.savefig('simulated_dilution_simple.pdf', bbox_inches='tight')
+
+stats
+# %%
+with pm.Model() as model:
+    like = mwc.bayes.DeterminsticCalibrationFactor(
+        'alpha', alpha_true * df['n1'].values),
+                                                   alpha_true * \
+                                                       df['n2'].values,
+                                                   testval = 1)
+    trace=pm.sample(draws = 10000, tune = 10000)
+    trace_df=mwc.stats.trace_to_dataframe(trace, model)
+    stats=mwc.stats.compute_statistics(trace_df)
+
+log_post=np.abs(log_post)
+x=log_post / log_post[-1]
+
+post=np.exp(-x) / np.sum(np.exp(-x))
+
+# %%
+fig, ax=plt.subplots(2, 1, figsize = (4.7, 3))
+ax[0].set_ylabel(r'$\propto g(\alpha\, \vert \, [I_1, I_2])$')
+ax[0].set_xlabel(r'$\alpha$ [a.u. / molecule]')
+ax[1].set_ylabel(r'$\alpha$ [a.u. / molecule]')
+ax[1].set_xlabel('step number')
+
+_=ax[0].hist(trace_df['alpha'], bins = 100, alpha = 0.8, edgecolor = 'k',
+               histtype='stepfilled', linewidth=0.8, normed=True,
+               label='__nolegend__')
+ypos = ax[0].get_ylim()[1] / 2
+_ = ax[0].plot(stats['mode'], ypos, 'o', color='firebrick', ms=5, label='mode')
+_ = ax[0].plot((stats['hpd_min'], stats['hpd_max']),(ypos, ypos),
+               color='firebrick', ms=5)
+_ = ax[1].plot(np.arange(0, len(trace_df), 1), trace_df['alpha'], alpha=0.6)
+ax[0].text(-0.18, 1.2, '(A)', fontsize=8, transform=ax[0].transAxes)
+ax[1].text(-0.18, 1.2, '(B)', fontsize=8, transform=ax[1].transAxes)
+sns.despine(offset=7)
+plt.tight_layout()
+plt.savefig('alpha_simple_mcmc.pdf', bbox_inches='tight')
+
+# %% Model 1
+sigma = 0.001 * alpha_true
+
+# Set the protein number range.
+prot_range = np.arange(10, 1000, 5)
+
+# Set up a dataframe.
+dfs = []
+cols = ['summed', 'fluct', 'I_1', 'I_2', 'n1', 'n2', 'ntot']
+for i, ntot in enumerate(prot_range):
+    n1 = np.random.binomial(ntot, p=0.5, size=num_div)
+    n2 = ntot - n1
+    I_1 = alpha_true * n1 + np.random.normal(0, sigma, size=num_div)
+    I_2 = alpha_true * n2 + np.random.normal(0, sigma, size=num_div)
+    summed = I_1 + I_2
+    fluct = (I_1 - I_2)**2
+    df = pd.DataFrame(np.array([summed, fluct, I_1, I_2, n1, n2,
+                                np.ones(num_div) * ntot]).T, columns=cols)
+    dfs.append(df)
+model1_df = pd.concat(dfs, ignore_index=True)
+
+
+# Perform the parameter estimation.
+with pm.Model() as model1:
+    alpha = pm.Uniform('alpha', lower=0, upper=1000)
+    sigma = mwc.bayes.Jeffreys('sigma', lower=0.001 * alpha_true, upper=0.1 * alpha_true)
+    n1 = pm.Uniform('n1', lower=1, upper=2000, shape=len(model1_df))
+    ntot = pm.Uniform('ntot', lower=1, upper=2000, shape=len(model1_df))
+
+
+    # Compute the means.
+    mu1 = alpha * n1
+    mu2 = alpha * (ntot - n1)
+
+    # Define the likelihoods.
+    like_1 = pm.Normal('like1', mu1, sd=sigma, observed=model1_df['I_1'].values)
+    like_2 = pm.Normal('like2', mu2, sd=sigma, observed=model1_df['I_2'].values)
+
+    # Burn with metropolis.
+    step = pm.Metropolis()
+    burn = pm.sample(tune=10000, draws=10000, step=step)
+
+    # Perform the sampling.
+    step = pm.NUTS()
+    model1_trace = pm.sample(tune=1000, draws=1000, step=step, start=burn[-1])
+    model1_trace_df = mwc.stats.trace_to_dataframe(model1_trace, model1)
+    model1_stats = mwc.stats.compute_statistics(model1_trace_df)
+
+# %%
