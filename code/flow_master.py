45d5c843c5e2c64d1ad77f20ef3b349603fed475
Zofii Kaczmarek <zofiikaczmarek@Zofiis-MacBook-Air.local>
2018-09-20 17:47:55 -0700

    working script to compile expt data

--- code/flow_master.py
+++ code/flow_master.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os #maybe not needed. 
 import sys
 sys.path.insert(0, '../')
+sys.path.insert(0, '../../work/PhillipsLab/git/mwc_growth')
 import mwc.io
 
 # Get the experiment paths.
@@ -21,49 +22,57 @@ n_added = 0
 for i, expt in enumerate(expt_paths):
     # Check status 
     state = mwc.io.scrape_frontmatter(expt)
-    if state['status'] == 'accepted':
+    if len(state) == 0:
+        print('skipped')
+    elif state['status'].lower() == 'accepted':
         # Read data
         if expt[-1] != '/':
             expt += '/'
-        _data = pd.read_csv('{}{}'.format(expt, 'output/*_flow_events.csv')
+        for f in glob.glob('{}output/*_flow_events.csv'.format(expt)): #expect only 1 file
+            _data = pd.read_csv(f)
 
         # Record any extra or missing columns, and append data if no extra columns
         diff_extra = _data.columns.difference(columns) #any extra column names
         diff_miss = columns.difference(_data.columns) #any missing column names
         if (not diff_extra.empty) | (not diff_miss.empty):
-            errors.append(expt.split('/')[-2] + 
-                            ' -> extra: {}, missing: {} %\n'.format(
+            errors.append('    ' + expt.split('/')[-2] + 
+                            ' -> extra: {}, missing: {} \n'.format(
                             diff_extra.values.tolist(), diff_miss.values.tolist()))
         if diff_extra.empty:
             _dfs.append(_data)
             n_added += 1
 
-df = pd.concat(_dfs, ignore_index=True)
-df = df[columns.tolist()]
+if (len(_dfs) != 0):
+    df = pd.concat(_dfs, ignore_index=True)
+    if (set(df.columns) == set(columns)):
+        df = df[columns.tolist()]
+    else:
+        print('Column(s) are missing, so column ordering was skipped.')
 
-summary = """
-Summary: ({} of {} experiments added)
-=============================================================================
-The following files are in error:\n
-""".format(n_added, len(expt_paths))
+    # Create error summary
+    summary = """
+    Summary: ({} of {} experiments added)
+    =============================================================================
+    The following files are in error:\n \n""".format(n_added, len(expt_paths))
 
-if len(errors) == 0:
-    failures = ['None\n']
-for e in errors:
-    summary += e
+    if len(errors) == 0:
+        failures = ['None\n']
+    for e in errors:
+        summary += e
 
-summary += """
------------------------------------------------------------------------------
-"""
+    summary += \
+    """    -----------------------------------------------------------------------------
+    """
 
-# Save dataframe and error summary to output.
-df.to_csv('../data/master_df.csv', index=False)
-#what should the csv name be? include the date it was compiled or anything? 
-#prob leave as is so new ones write over it? or maybe that's not ideal.
+    # Save dataframe and error summary to output.
+    df.to_csv('../data/master_df.csv', index=False)
 
-if os.path.isdir('../data/validation') == False: #where to put this summary? (we're sitting in code rn)
-    os.mkdir('../data/validation')
-with open('{}{}'.format('../data/validation/', 'flow_master_summary.txt'), 'w') as f:
-    f.write(output)
+    if os.path.isdir('../data/validation') == False:
+        os.mkdir('../data/validation')
+    with open('{}{}'.format('../data/validation/', 'flow_master_summary.txt'), 'w') as f:
+        f.write(summary)
 
-print(summary) #what's proper here? nothing at all?
+    print(summary)
+
+else:
+    print('No dataframes were compiled.')
