367260f5704676d651fe40ad8e47a6b3bbdc8954
Griffin Chure <gchure@caltech.edu>
2018-10-17 11:21:11 -0700

    Modified a segmentation script

--- code/processing/microscopy/20181016_r2_37C_glycerol_O2_dilution/segmentation.m
+++ code/processing/microscopy/20181016_r2_37C_glycerol_O2_dilution/segmentation.m
@@ -3,10 +3,10 @@
 % Define the experiment parameters.
 DATE = '20181016';
 BASENAME = 'r2_37C_glycerol_O2';
-samples = {'growth', 'snaps'};
+samples = {'growth'};
 
 % Get the snaps names.
-dirpath = ['../../../../data/images/', DATE, '_', BASENAME, '/snaps/']
+dirpath = ['../../../../data/images/', DATE, '_', BASENAME, '_dilution/snaps/'];
 snap_files = dir(dirpath);
 snap_samples = {snap_files.name};
 ignored = {'.', '..', '.DS_Store', 'snaps'};
@@ -16,26 +16,24 @@ ignored = {'.', '..', '.DS_Store', 'snaps'};
           valid = valid + strcmp(snap_samples{i}, ignored{j});
       end
       if valid == 0
-          samples{end+1} = snap_samples{i};
+          samples{end+1} = ['snaps/', snap_samples{i}];
           end
   end
 
 CONST = loadConstants('100XEc');
-CONST.parallel.PARALLEL_FLAG = 1;
-% (* CONST.parallel.xy_parallel = 1; *)
 CONST.trackFoci.numSpots = 0;
 CONST.align.ALIGN_FLAG = 1;
 CONST.trackOpti.REMOVE_STRAY = 1;
 cleanFlag = 0;
 % parpool(2, 'IdleTimeout', 1000000)
-for i=2:length(samples)
+for i=1:length(samples)
     disp(samples{i})
     statement = ['Beginning segmentaton ', num2str(i), ' out of ',...
         num2str(length(samples))];
     disp(statement)
 
     % Define the data directory.
-    directory = ['../../../../data/images/', DATE, '_', BASENAME, samples{i}]
+    directory = ['../../../../data/images/', DATE, '_', BASENAME, '_dilution/', samples{i}]
 
 
     % Perform the segmentation.
@@ -43,4 +41,4 @@ for i=2:length(samples)
 delete(gcp('nocreate'));
 end
 disp('Finished!');
-% (* exit(); *)
+exit();
