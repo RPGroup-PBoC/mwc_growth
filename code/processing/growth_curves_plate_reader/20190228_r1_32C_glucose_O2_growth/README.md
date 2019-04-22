65ab8567b2887936c82c57683cac8b51f326d93d
Zofii Kaczmarek <zofiikaczmarek@Zofiis-MacBook-Air.local>
2019-04-22 11:20:10 -0700

    current state of affairs

--- code/processing/growth_curves_plate_reader/20190228_r1_32C_glucose_O2_growth/README.md
+++ code/processing/growth_curves_plate_reader/20190228_r1_32C_glucose_O2_growth/README.md
@@ -1,6 +1,6 @@
 ---
-status: Rejected
-reason: experiment not yet completed
+status: Accepted
+reason: Everything looks good
 ---
 
 # 2019-02-28 32C Glucose O2 Plate Reader Growth Measurement
@@ -8,17 +8,29 @@ reason: experiment not yet completed
 ## Purpose
 This experiment aims to measure the growth rate of the *E. coli* strains of interest in M9 + 0.5% glucose at 32°C.
 
+## Strain Information
+
+| Location | Plasmid | Genotype | Host Strain | Shorthand |
+| :------- | :------ | :------- | ----------: | --------: |
+| dilution 1 - pos. 9 | `pZS3*PN25-tetR`| `galK<>25O2+11-YFP, gspI<>4*5noO1v1-CFP` |  HG105 |`deltaLacI` |
+
 ## Notes & Observations
-* A `deltaLacI` LB culture was grown to saturation, then diluted 1000x into a 96 well plate of growth media immediately prior to start of measurement.
-* Measurements were taken in the Van Valen Lab Biotek Gen5 plate reader.
-* The 96 well plate used had square wells and a total capacity of 700µL, with 300µL of culture added to each well.
-* The plate was shaken in a linear mode at the fastest speed for the duration of the experiment.
-* Measurements were taken every 7 min for 17 hours.
+
 
 ## Analysis Files
 
-**Plate Layout**
-![plate layout](output/growth_plate_layout.png)
+**Whole Plate Growth Curves**
+![plate layout](output/delta_glucose/gp_output_curves.png)
 
-**Growth Curves**
-[![growth curves](output/growth_plate_summary.png)]
+**Per Well Growth Rate Heatmap**
+[![growth curves](output/delta_glucose/per_well_doubling_times_heatmap.png)]
+
+## Experimental Protocol
+
+1. Cells as described in "Strain Information" were grown to saturation in 3mL of LB Miller.
+
+2. Cells were diluted 1:1000 into 10mL of growth media in a 14mL Falcon tube.
+
+3. 300µL of water were added to the first and last two rows and columns of a square-welled, clear-bottomed 96 well plate, the total capacity of which was 700µL. 300µL of cells were added to the remaining wells.
+
+4. The plate was placed in a Biotek Gen5 plate reader and grown at 32C, shaking in a linear mode at the fastest speed. Measurements were taken every 7 minutes for approximately 17 hours.
--- templates/growth_plate_reader_analysis.py
+++ templates/growth_plate_reader_analysis.py
@@ -12,13 +12,21 @@ import mwc.viz
 colors = mwc.viz.personal_style()
 import mwc.fitderiv
 import seaborn as sns
+import statsmodels.api as sm
 
 # Define the experimental constants
+# For figure titles
+DATE = 
+RUN_NO = 
+TEMP = 
+# For analysis
 STRAIN = ''
 CARBON = ''
 
-WHOLE_PLATE = False
-PER_WELL = False
+WHOLE_PLATE = True
+WHOLE_REPLOT_ONLY = False #only replots if whole plate is also true
+PER_WELL = True
+PER_WELL_REPLOT_ONLY = False
 
 # ----------------------------------
 
@@ -48,28 +56,30 @@ if os.path.exists(f'output/{STRAIN}_{CARBON}/') == False:
 
 # Perform full plate analysis
 if WHOLE_PLATE:
+    if not WHOLE_REPLOT_ONLY:
+        # Using the package [`fitderiv`](http://swainlab.bio.ed.ac.uk/software/fitderiv/) from Peter Swain's lab, 
+        # perform non-parametric inference of the time-dependent growth rates.
+        gp = mwc.fitderiv.fitderiv(restricted['time_min'].values, restricted['od_sub'].values)
 
-    # Using the package [`fitderiv`](http://swainlab.bio.ed.ac.uk/software/fitderiv/) from Peter Swain's lab, 
-    # perform non-parametric inference of the time-dependent growth rates.
-    gp = mwc.fitderiv.fitderiv(restricted['time_min'].values, restricted['od_sub'].values)
+        # Export summary statistics
+        stats = gp.printstats(performprint=False)
+        _stats = pd.DataFrame(list(stats.items())).rename(columns = {0:'parameter',1:'value'})
+        _stats.to_csv(f'output/{STRAIN}_{CARBON}/gp_output_stats.csv')
 
-    # Export summary statistics
-    stats = gp.printstats(performprint=False)
-    _stats = pd.DataFrame(list(stats.items())).rename(columns = {0:'parameter',1:'value'})
-    _stats.to_csv(f'output/{STRAIN}_{CARBON}/gp_output_stats.csv')
-
-    # Create dataframe with full time series results of the fit
-    gp_df = gp.export('NONE', savegp = False, savestats = False)
-    # Calculate doubling time
-    gp_df = gp_df[['t','od','log(OD)','log(OD) error','gr','gr error']]
-    gp_df.rename(columns = {'log(OD)':'log(OD)_fit', 'log(OD) error':'log(OD)_fit_std', 
-                            'gr':'growth_rate', 'gr error':'growth_rate_std', 
-                            'od':'OD_raw_data', 't':'time'}, inplace = True)
-    gp_df['doubling_time'] = np.log(2)/gp_df['growth_rate']
-    gp_df['doubling_time_std'] = np.log(2)*gp_df['growth_rate_std']/(gp_df['growth_rate']**2)
-    # Save results
-    gp_df.to_csv(f'output/{STRAIN}_{CARBON}/gp_output.csv')
-
+        # Create dataframe with full time series results of the fit
+        gp_df = gp.export('NONE', savegp = False, savestats = False)
+        # Calculate doubling time
+        gp_df = gp_df[['t','od','log(OD)','log(OD) error','gr','gr error']]
+        gp_df.rename(columns = {'log(OD)':'log(OD)_fit', 'log(OD) error':'log(OD)_fit_std', 
+                                'gr':'growth_rate', 'gr error':'growth_rate_std', 
+                                'od':'OD_raw_data', 't':'time'}, inplace = True)
+        gp_df['doubling_time'] = np.log(2)/gp_df['growth_rate']
+        gp_df['doubling_time_std'] = np.log(2)*gp_df['growth_rate_std']/(gp_df['growth_rate']**2)
+        # Save results
+        gp_df.to_csv(f'output/{STRAIN}_{CARBON}/gp_output.csv')
+    else:
+        gp_df = pd.read_csv(f'output/{STRAIN}_{CARBON}/gp_output.csv')
+    
     # Plot results
     fig, ax = plt.subplots(ncols = 3, figsize=(10, 4))
     ax[0].set_title('growth vs time', fontsize = 10)
@@ -100,53 +110,57 @@ if WHOLE_PLATE:
 
 # Perform by-well analysis
 if PER_WELL:
-    
-    well_stats = []
-    well_gps = []
-    
-    for well in restricted['well_id'].unique():
-        # Select data for single well
-        subset = restricted[restricted['well_id'] == well]
+    if not PER_WELL_REPLOT_ONLY:
+        well_stats = []
+        well_gps = []
+
+        for well in restricted['well_id'].unique():
+            # Select data for single well
+            subset = restricted[restricted['well_id'] == well]
+
+            # Run gaussian processing and export fit and time derivative results
+            gp = mwc.fitderiv.fitderiv(subset['time_min'].values, subset['od_sub'].values)
+            gp_df = gp.export('NONE', savegp = False, savestats = False)
+
+            # Append summary statistics
+            stats = gp.printstats(performprint=False)
+            well_stats.append(pd.DataFrame(list(stats.items())).assign(well_id = well))
+
+            # Calculate doubling time and add to gp results, then append dataframe to list of gp results
+            gp_df = gp_df.assign(well_id = well)
+            gp_df = gp_df[['well_id','t','od','log(OD)','log(OD) error','gr','gr error']]
+            gp_df.rename(columns = {'log(OD)':'log(OD)_fit', 'log(OD) error':'log(OD)_fit_std', 
+                                    'gr':'growth_rate', 'gr error':'growth_rate_std', 
+                                    'od':'OD_raw_data', 't':'time'}, inplace = True)
+            gp_df['doubling_time'] = np.log(2)/gp_df['growth_rate']
+            gp_df['doubling_time_std'] = np.log(2)*gp_df['growth_rate_std']/(gp_df['growth_rate']**2)
+            well_gps.append(gp_df)
         
-        # Run gaussian processing and export fit and time derivative results
-        gp = mwc.fitderiv.fitderiv(subset['time_min'].values, subset['od_sub'].values)
-        gp_df = gp.export('NONE', savegp = False, savestats = False)
-        
-        # Append summary statistics
-        stats = gp.printstats(performprint=False)
-        well_stats.append(pd.DataFrame(list(stats.items())).assign(well_id = well))
-        
-        # Calculate doubling time and add to gp results, then append dataframe to list of gp results
-        gp_df = gp_df.assign(well_id = well)
-        gp_df = gp_df[['well_id','t','od','log(OD)','log(OD) error','gr','gr error']]
-        gp_df.rename(columns = {'log(OD)':'log(OD)_fit', 'log(OD) error':'log(OD)_fit_std', 
-                                'gr':'growth_rate', 'gr error':'growth_rate_std', 
-                                'od':'OD_raw_data', 't':'time'}, inplace = True)
-        gp_df['doubling_time'] = np.log(2)/gp_df['growth_rate']
-        gp_df['doubling_time_std'] = np.log(2)*gp_df['growth_rate_std']/(gp_df['growth_rate']**2)
-        well_gps.append(gp_df)
-        
-    # Compile dataframe of GP results for all wells
-    well_data = pd.concat(well_gps)
-    well_data['carbon'] = CARBON
-    well_data['strain'] = STRAIN
-    well_data = well_data.sort_values('well_id')
-    well_data.to_csv(f'output/{STRAIN}_{CARBON}/per_well_output.csv', index=False)
-    
-    # Compile dataframe of statistics for all wells
-    well_stats_df = pd.concat(well_stats)
-    well_stats_df = well_stats_df.rename(columns = {0:'parameter',1:'value'})
-    well_stats_df['carbon'] = CARBON
-    well_stats_df['strain'] = STRAIN
-    well_stats_df = well_stats_df.sort_values('well_id')
-    well_stats_df.to_csv(f'output/{STRAIN}_{CARBON}/per_well_stats.csv', index=False)
+        # Compile dataframe of GP results for all wells
+        well_data = pd.concat(well_gps)
+        well_data['carbon'] = CARBON
+        well_data['strain'] = STRAIN
+        well_data = well_data.sort_values('well_id')
+        well_data.to_csv(f'output/{STRAIN}_{CARBON}/per_well_output.csv', index=False)
+
+        # Compile dataframe of statistics for all wells
+        well_stats_df = pd.concat(well_stats)
+        well_stats_df = well_stats_df.rename(columns = {0:'parameter',1:'value'})
+        well_stats_df['carbon'] = CARBON
+        well_stats_df['strain'] = STRAIN
+        well_stats_df = well_stats_df.sort_values('well_id')
+        well_stats_df.to_csv(f'output/{STRAIN}_{CARBON}/per_well_stats.csv', index=False)
+
+    else:
+        well_data = pd.read_csv(f'output/{STRAIN}_{CARBON}/per_well_output.csv')
+        well_stats_df = pd.read_csv(f'output/{STRAIN}_{CARBON}/per_well_stats.csv')
 
     alpha_map = {alpha:no for alpha, no in zip(string.ascii_uppercase, np.arange(0, 27, 1) * 12)}
     alphanumeric_map = {f'{a}{n}':alpha_map[a] + n for n in np.arange(1, 13, 1) for a in string.ascii_uppercase}
     row_letters = {no:alpha for no, alpha in zip(np.arange(0, 27, 1),string.ascii_uppercase)}
 
     # Plot doubling time curves for all wells
-    fig, ax = plt.subplots(8,12, figsize=(10, 4))
+    fig, ax = plt.subplots(8,12, figsize=(10, 4), sharex=True) #,sharey=True
 
     for r in np.arange(0,8,1):
         for c in np.arange(0,12,1):
@@ -160,7 +174,10 @@ if PER_WELL:
                                       data['doubling_time']+data['doubling_time_std'],
                                       facecolor= 'blue', alpha= 0.2)
                 ax[r][c].axvline(x=int(data[data['doubling_time'] == data['doubling_time'].min()]['time']),c='red')
-    ax[0][5].set_title('doubling time vs time', fontsize = 10)
+                ax[r][c].set_ylim([0, 300])
+    ax[0][0].set_ylim([0, 300])
+    ax[0][0].get_yaxis().set_visible(True)
+    fig.suptitle(f'{DATE}_r{RUN_NO}_{TEMP}C_{CARBON}_{STRAIN} doubling time vs time', fontsize = 10, y=0.93)
     plt.savefig(f'output/{STRAIN}_{CARBON}/per_well_doubling_time_curves.png')    
     plt.close()
 
@@ -175,16 +192,20 @@ if PER_WELL:
                 plate[r][c] = int(_temp[_temp['parameter'] == 'inverse max df']['value'])
     mask = np.isnan(plate)
     ax = sns.heatmap(plate, mask=mask, square = True)
-    ax.set_title('doubling times', fontsize = 10)
+    ax.set_title(f'{DATE}_r{RUN_NO}_{TEMP}C_{CARBON}_{STRAIN} doubling times', fontsize = 10)
     plt.savefig(f'output/{STRAIN}_{CARBON}/per_well_doubling_times_heatmap.png')    
     plt.close()
 
     # Plot distribution of minimum doubling times for all wells
     fig, ax = plt.subplots()
-    ax.hist(well_stats_df[well_stats_df['parameter']=='inverse max df']['value'], bins = 20, color= 'tomato')
+    dat = well_stats_df[well_stats_df['parameter']=='inverse max df']['value']
+    ecdf = sm.distributions.ECDF(dat)
+    x = np.linspace(min(dat), max(dat))
+    y = ecdf(x)
+    ax.step(x, y)
     ax.set_xlabel('doubling time (minutes)', fontsize = 10)
     ax.set_ylabel('frequency', fontsize = 10)
-    ax.set_title('doubling time distribution across wells', fontsize = 12)
+    ax.set_title(f'{DATE}_r{RUN_NO}_{TEMP}C_{CARBON}_{STRAIN} doubling time distribution across wells', fontsize = 10)
     ax.tick_params(labelsize=10)
     plt.savefig(f'output/{STRAIN}_{CARBON}/per_well_doubling_times_distribution.png')
     plt.close()
